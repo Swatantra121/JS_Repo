48532f6424cd960ab67c7ca503d027d79ffe55ec
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-12 18:45:35 +0000

    conversion of datatype

---
+++ 01_basics/conversationOperator.js
@@ -0,0 +1,11 @@
+let score ="33";
+console.log(typeof score);
+console.log(typeof (score));
+let valueNumber =Number(score);
+console.log(typeof valueNumber);
+// "33" => convert into 33 
+// "33adad"  => not convert in number its give in NaN
+// true => 1; false => false 0;
+ let isLogedIn =1;
+ let booleanIsLogedIn = Boolean(isLogedIn);
+ console.log(booleanIsLogedIn);
