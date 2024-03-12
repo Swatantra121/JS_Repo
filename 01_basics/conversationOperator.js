ba9dfa2c98e034f316997722f97e41908088ea3b
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-12 19:02:05 +0000

    operation conversasion

--- 01_basics/conversationOperator.js
+++ 01_basics/conversationOperator.js
@@ -1,11 +1,31 @@
 let score ="33";
-console.log(typeof score);
-console.log(typeof (score));
-let valueNumber =Number(score);
-console.log(typeof valueNumber);
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueNumber =Number(score);
+// console.log(typeof valueNumber);
 // "33" => convert into 33 
 // "33adad"  => not convert in number its give in NaN
 // true => 1; false => false 0;
  let isLogedIn =1;
  let booleanIsLogedIn = Boolean(isLogedIn);
- console.log(booleanIsLogedIn);
+//  console.log(booleanIsLogedIn);
+
+ ///***************** Operations **************************/
+ let value  =3 ;
+ let negValue =- value ;
+ console.log(negValue);
+
+
+  console.log(+true);
+  console.log(+"");
+
+  
+  let  numb1 , numb2, numb3;
+  numb1= numb2= numb3 =2+2;
+
+  let  gameCounter =100;
+  let  gameCounter1 =200;
+  gameCounter ++;
+  ++gameCounter1
+  console.log(gameCounter);
+  console.log(gameCounter1);
