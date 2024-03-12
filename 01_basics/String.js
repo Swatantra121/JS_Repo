20718ba55c62255dbc4acd01b37255d06cbc9e31
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-12 20:09:10 +0000

    string concepts

---
+++ 01_basics/String.js
@@ -0,0 +1,22 @@
+const name = "swat";
+const repoCount =50;
+console.log(name +repoCount);
+console.log(`my name is ${name} repo is ${repoCount}`);
+  const gameName = new String('helloram')
+  console.log(gameName)
+  console.log(gameName[0])
+  console.log(gameName.charAt(2))
+  console.log(gameName.indexOf('e'))
+   const newString = gameName.substring(0,4);
+   console.log(newString);
+   const newString1 =gameName.slice(0,5);
+   console.log(newString1);
+   const newStringTrim = '    asdas   '
+   console.log(newStringTrim.trim());
+    const stringReplace = "shyamramer%45";
+    console.log(stringReplace.replace('%45','nn'));
+    const str = "hello";
+    const reversedStr = str.split("").reverse().join("");
+    
+    console.log(reversedStr); // "olleh"
+
