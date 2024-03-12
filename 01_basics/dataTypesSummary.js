7dcf868bfc351ed3759632f6a02955f0d7cf2f58
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-12 19:26:55 +0000

    data type summary

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,12 @@
+// primitive data type 
+// 7 catogory  : String , Number , Boolean , null , undefined , symbol, BigInt,
+const id =Symbol('123');
+const newId =Symbol('123');
+console.log( id ==newId);
+
+
+
+
+//Reference (Non primitive )
+
+// Array , Object , Functions 
