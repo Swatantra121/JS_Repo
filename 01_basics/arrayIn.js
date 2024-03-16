7385ea94b479ca2d0b0e566efadc46e83c14fcef
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-16 13:00:46 +0000

    array in javascript

---
+++ 01_basics/arrayIn.js
@@ -0,0 +1,29 @@
+// Array
+const myArr=[1,2,3,4,5];
+ const myArr2 =new Array(1,2,3 ,3);
+
+//  console.log(myArr2[2])
+
+ //array methods
+
+ //myArr.push(6); // add the element in array 
+// myArr.pop()// remove the last element in array 
+// myArr.unshift(9) add the element in start in array 
+// myArr.shift() remove the first element  in array 
+// console.log(myArr.includes(9));// check the Element it is in the array  
+// console.log(myArr.indexOf(3));  // check the where is the Element in array
+
+const newArray =myArr.join()// its convert  in string format in array 
+// console.log(myArr);
+// console.log(newArray); 
+
+// slice , splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+const myn2 = myArr.splice(1,3);
+console.log(myn2)
+
+ 
