79eb3d2b8e8e72c297e63028a3100b2fe65dce8f
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-17 19:38:59 +0000

    controlls

---
+++ 02_controlflow/one.js
@@ -0,0 +1,26 @@
+// const isUserLogedIn4= true;
+
+// if(2==="2"){
+//     console.log("executed");
+
+// }
+// else{
+//     console.log("error");
+// } 
+
+const month =3
+switch(month){
+    case 1:
+        console.log("feb")
+        break;
+        case 3:
+            console.log("march")
+            break;
+            case 4: 
+            console.log("april");
+            break;
+            default:
+            console.log("default case");
+            break;
+
+}
---
+++ 02_controlflow/trueandfalase.js
@@ -0,0 +1,34 @@
+const userEmail ="swat@gma.com";
+// if(userEmail){
+//     console.log("got the mail");
+
+// }
+// else{
+//     console.log("dont have email");
+// }
+
+//falsy  value 
+//false ,o,-0,bigint , 0n,"", null, undefined, NaN
+
+//truthy value
+
+// "0" ,"false", " ", [],{},function(){}
+ if (userEmail.length===0){
+    console.log("array is empty");
+
+ }
+//  const emptyObject ={};
+
+//  if(Object.keys(emptyObject).length===0){
+//     console.log("object is empty")
+// }
+
+// nullish coalescing operator (??) null undefined
+//  let val1 
+//  val1 = null ?? 10 //null coalescion  operator
+//  val1 = null ?? 10
+//  val1 = undefined ?? 10  
+// val1 = undefined ?? 100?? 10
+//  console.log(val1);
+
+ 
