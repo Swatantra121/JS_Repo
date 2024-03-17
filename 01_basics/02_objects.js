9b7cd0542ae08e2fa95de39a56771953337864eb
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-17 08:29:17 +0000

    destructure

--- 01_basics/02_objects.js
+++ 01_basics/02_objects.js
@@ -4,17 +4,26 @@ const tinderuser =new Object() //this is singleton object
 tinderuser.email= "gma@fma.com",
 tinderuser.name ="swat",
 tinderuser.id=232
- console.log(tinderuser)
+//  console.log(tinderuser)
 const obj1 ={1:"a",2:"b",3:"c"};
 const obj2 ={4:"d",5:"e",6:"f"};
 // const obj3 ={obj1 ,obj2};
 
 //const obj3  = Object.assign({},obj1,obj2)//with the help of merge 2 object in single object
 const obj3 ={...obj1,...obj2} 
-console.log(obj3);  
+// console.log(obj3);  
 
-console.log(Object.keys(tinderuser))// find the keys 
-console.log(Object.values(tinderuser)); //find the values
+// console.log(Object.keys(tinderuser))// find the keys 
+// console.log(Object.values(tinderuser)); //find the values
 
-console.log(Object.entries(tinderuser)) //convert into every key value in array
-console.log(tinderuser.hasOwnProperty("email")) // find the key  in object 
+// console.log(Object.entries(tinderuser)) //convert into every key value in array
+// console.log(tinderuser.hasOwnProperty("email")) // find the key  in object 
+
+const course ={
+    courseInstructor: "ram",
+    price: 200,
+    courseName: "javascript"
+}
+
+const {courseInstructor : instructor}=course;  // object destructure 
+console.log(instructor);
