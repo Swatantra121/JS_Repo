f1c7b07f3b23ba87deb3ce5108a7742a402a64b5
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-17 17:22:55 +0000

    iife

---
+++ 01_basics/Jsfunction/iife.js
@@ -0,0 +1,16 @@
+// immediately Invoked Funtion expressions (IIFE)
+
+(function  swat(){ 
+    // name iife
+    console.log("db connected");
+
+})();
+
+
+//useing arrow funciton 
+
+(()=>{
+    console.log("DB connected 2")
+})();
+
+// remove the global scope polution  when we use  IIFE 
