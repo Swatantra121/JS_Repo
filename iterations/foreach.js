a566d13215564689ea468769f5190e6870f38f16
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-20 18:38:44 +0000

    for each and reduce

--- iterations/foreach.js
+++ iterations/foreach.js
@@ -1,1 +1,31 @@
 // for each loop
+const codeing =["js","java", "python"];
+// codeing.forEach( (item)=>{
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+// codeing.forEach(printMe)
+
+// codeing.forEach((item ,index , arr)=>{
+//     console.log(item, index , arr);
+// })
+ const myCoding =[
+    {
+    langName: "javaScript",
+    filename: "js"
+ },
+ {
+    langName: "html",
+    filename: "html"
+ },
+ {
+    langName: "python",
+    filename: "py"
+ }]
+
+ myCoding.forEach((item)=>{
+    console.log(item);
+ })
