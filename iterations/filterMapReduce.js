f094e15105ff6cdb280ebfc57c8e3cbe4d1e956c
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-03-20 18:40:23 +0000

    reduce

---
+++ iterations/filterMapReduce.js
@@ -0,0 +1,60 @@
+// const codeing =["js","java", "python"];
+// const value =codeing.forEach((item)=>{ // for each never return value in variaable 
+// console.log(item);
+// })
+// console.log(value);
+
+const myNum=[1,2,3,4,5,6,7,8,9];
+const newNum = myNum.filter((num)=> num>4)
+// console.log(newNum);
+ const newNumber =[]
+ myNum.forEach( // how to return value in  with for each  loop
+    (num)=>{
+        if (num >0){
+            newNumber.push(num)
+        }
+    }
+ )
+//  console.log(newNumber);
+
+///  /////////////////map///////////////////////
+
+
+// let newvalues = myNum.map((num)=>num+10)
+
+
+//chaning in  method 
+
+// let newvalues = myNum.map((num)=>num*10)
+// .map((num)=>num+1)
+// .filter((num)=>num>=4)
+
+// console.log(newvalues);
+///////////////////////////reduce ///////////////////////
+
+// let totalvalue =myNum.reduce(function(acc, currval){
+//     console.log(`acc:${acc} currval: ${currval}`);
+//     return acc+currval
+// },0)
+// console.log(totalvalue);
+
+const shoppingCart= [
+    {
+        productname: "apple mobile",
+        price: 50000
+
+    },
+    {
+        productname: "sumsung mobile",
+        price: 5000
+
+    },
+    {
+        productname: "nokia mobile",
+        price: 10000
+
+    }
+] 
+
+const priceToPay = shoppingCart.reduce((acc, item)=>acc+item.price,0);
+console.log(priceToPay);
