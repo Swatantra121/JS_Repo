4c9b10515e3f6671bb4fb96ec1b158b0a642dec0
Swatantra <90821061+Swatantra121@users.noreply.github.com>
2024-07-29 18:40:20 +0000

    new test

--- interivews/test1.js
+++ interivews/test1.js
@@ -2,6 +2,16 @@ let names = "hello"
 console.log(names.split("").reverse("").join(""))
 const convertArry = names.split("")
 
+
+// revers array and  remove the first alphabtes and put in the last  
+let convertInArray = names.split("")
+  let removeAlpa = convertInArray.shift();  // remove the first char in the array 
+  convertInArray.push(removeAlpa) // add the  char  in the last
+  let result = convertArry.join('') // join the string
+  console.log(result);
+
+
+
 for (i = 0; i < convertArry.length; i++) {
     console.log(convertArry[i]);
 
@@ -71,3 +81,24 @@ a=b-a;
 b=b-a;
 
 console.log(`a:${a} b${b}`);
+
+
+// count alphabates  in array how many time repeat 
+
+ let arr =['c','s','c','s','q','a','q','a',];
+ let coun ={}
+ arr.forEach(e => {
+
+  // for(let obj   of e){
+     e= e.toLowerCase();
+    if(e >='a' && e <='z'){
+      if(coun[e]){
+        coun[e]++
+      }
+      else{
+        coun[e] =1
+      }
+    // }
+  }  
+ });
+ console.log(coun);
